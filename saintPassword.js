2dc11b9bae729048264b543fb20b035bb0985e3e
ryanlanesander <ryanlanesander@gmail.com>
2024-06-26 01:20:02 -0400

    Style Update!
    - CSS added for new fonts
    - Spacing changed on blog posts
    - New background mimicking watercolor paper
    - Need to find good watercolor font
    - Added options to project tab
    - New look for navbar title

--- saintPassword.js
+++ saintPassword.js
@@ -9,31 +9,3 @@ function checkPassword() {
         alert('Incorrect password. Please try again.');
     }
 }
-
-document.addEventListener('DOMContentLoaded', () => {
-    const app = new PIXI.Application({ width: 800, height: 600, backgroundColor: 0x1099bb });
-    document.getElementById('cube-container').appendChild(app.view);
-
-    const cube = new PIXI.Graphics();
-    cube.beginFill(0xde3249);
-    cube.drawRect(-50, -50, 100, 100);
-    cube.endFill();
-    cube.x = app.screen.width / 2;
-    cube.y = app.screen.height / 2;
-    cube.interactive = true;
-    cube.buttonMode = true;
-
-    app.stage.addChild(cube);
-
-    let spinning = false;
-
-    cube.on('pointerdown', () => {
-        spinning = !spinning;
-    });
-
-    app.ticker.add((delta) => {
-        if (spinning) {
-            cube.rotation += 0.1 * delta;
-        }
-    });
-});
